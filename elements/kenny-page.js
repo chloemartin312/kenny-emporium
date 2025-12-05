0a2ba59e0e1ff9659a164573696d47876d5a26b3
Damon Cagliola <dcagliola@gmail.com>
2025-12-05 13:09:22 -0500

    Create kenny-page.js with component setup
    Initial setup of the Kenny page component with imports.

---
+++ elements/kenny-page.js
@@ -0,0 +1,8 @@
+/*
+Component to use for each page
+*/
+import { LitElement, html, css } from "lit";
+import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
+import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
+import "./kenny-social.js";
+import "./kenny-banner.js";
